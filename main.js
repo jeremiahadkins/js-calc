f62d1a8632793f6ec73cee2f369017f589735745
Jeremiah Adkins <hi@jeremiahadkins.com>
2017-08-03 23:20:53 -0400

    poor attempt at plus/minus

--- main.js
+++ main.js
@@ -4,15 +4,17 @@
   var operatorButton = document.querySelectorAll('.calc-operator');
   var equalButton = document.querySelector('.calc-equals');
   var clearButton = document.querySelector('.calc-clear');
+  var reverseNumButton = document.querySelector('.calc-reverse');
   var displayArea = document.querySelector('.calc-header');
   var currentNum = '';
-  var currentOp = '';
   var calculation = [];
   var isCleared = false;
   var operator = '';
+  var displayText = displayArea.textContent;
 
   equalButton.addEventListener("click", calculate);
   clearButton.addEventListener("click", clearDisplay);
+  reverseNumButton.addEventListener("click", reverseNum);
 
   for (var i = 0; i < numberButton.length; i++) {
     numberButton[i].addEventListener("click", pushNumber);
@@ -64,9 +66,19 @@
       var answer = calculation[0] / calculation[1];      
     }
     console.log(answer);
+    display(answer);
     return answer;
+    calculation = '';
   }
 
+  function reverseNum(){
+    if (displayArea.textContent === 0) {
+    } else if (displayArea.textContent > 0) {
+      displayArea.textContent = -displayArea.textContent;
+    } else if (displayArea.textContent < 0) {
+      displayArea.textContent = +displayArea.textContent;
+    }
+  }
 
   function clearDisplay() {
     currentNum = 0;
